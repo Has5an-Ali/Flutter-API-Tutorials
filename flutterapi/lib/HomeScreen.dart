8e1059ef2e9324e73b6314b44659c96c669b01f3
Hassan Ali <hassanali22857@gmail.com>
2024-06-24 17:39:55 +0500

    complete

--- flutterapi/lib/HomeScreen.dart
+++ flutterapi/lib/HomeScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -8,6 +11,27 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  Map<String, dynamic>? dataMap;
+  Map<String, dynamic>? Donedata;
+  Future API() async {
+    http.Response response;
+    response = await http.get(Uri.parse('https://reqres.in/api/users/2'));
+    if (response.statusCode == 200) {
+      setState(() {
+        dataMap = jsonDecode(response.body);
+        Donedata = dataMap!['data'];
+        print(Donedata);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    API();
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +43,20 @@ class _HomescreenState extends State<Homescreen> {
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
+      body: Center(
+          child: Donedata == null
+              ? const CupertinoActivityIndicator()
+              : ListTile(
+                  title: Center(
+                    child: Text(
+                      Donedata!['first_name'].toString() +
+                          " " +
+                          Donedata!['last_name'].toString() +
+                          " \n " +
+                          (Donedata!['email'].toString()),
+                    ),
+                  ),
+                )),
     );
   }
 }
